4c583b70a991d3e3da9db8ea4f92e51b1ca681af
Christopher Reinken <christopherreinken@gmail.com>
2020-03-25 15:09:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  string = ""
+  case array.length
+      when 1
+        string << array.join()
+      when 2
+        string << array.join(" and ")
+      else
+        string << array[0...-1].join(", ") + ", and " + array.last
+    end
+    
+    return string
 end
